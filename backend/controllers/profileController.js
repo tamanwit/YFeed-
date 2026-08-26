3e44884d58d188e79fbdb3e20d6c851f9bab5dbf
taman.js <tamanwit.25bcs10001@sst.scaler.com>
2026-08-27 04:45:33 +0530

    unfollow logic implemented

--- backend/controllers/profileController.js
+++ backend/controllers/profileController.js
@@ -112,9 +112,41 @@ const followProfile = async (req, res) => {
   }
 };
 
+const unfollowProfile = async (req, res)=>{
+  try {
+    const { username } = req.params;
+    const userId = req.user.id;
+    const targetUser = await User.findOne({ username });
+    if (!targetUser) {
+      return res.status(404).json({ message: "Username Not Found" });
+    }
+    if (targetUser._id.toString() === userId) {
+      return res.status(400).json({ message: "You cannot unfollow yourself" });
+    }
+    // Atomic update for target user
+    await User.findByIdAndUpdate(targetUser._id, {
+      $pull: { followers: userId },
+    });
+    // Atomic update for logged-in user
+    await User.findByIdAndUpdate(userId, {
+      $pull: { followings: targetUser._id },
+    });
+
+    return res.status(200).json({
+      message: `${username} is being not followed by ${req.user.username}`,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
+}
+
 module.exports = {
   getUserProfile,
   updateUserProfile,
   getOtherAccount,
   followProfile,
+  unfollowProfile,
 };
--- backend/routes/profileRouter.js
+++ backend/routes/profileRouter.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const verifyToken = require("../middlewares/middleware");
-const { getUserProfile, updateUserProfile, getOtherAccount, followProfile } = require("../controllers/profileController");
+const { getUserProfile, updateUserProfile, getOtherAccount, followProfile, unfollowProfile } = require("../controllers/profileController");
 
 // protected route
 router.get("/profile", verifyToken, getUserProfile);
 router.put("/profile/update", verifyToken, updateUserProfile);
 router.get("/profile/:username", verifyToken, getOtherAccount);
 router.put("/profile/follow/:username", verifyToken, followProfile)
-
+router.put("/profile/unfollow/:username", verifyToken, unfollowProfile)
 
 module.exports = router;
