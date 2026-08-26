3d37b5f6ba73bf0ada38d67eccac8962e19e17ad
taman.js <tamanwit.25bcs10001@sst.scaler.com>
2026-08-26 14:20:22 +0530

    fetch other user account

--- backend/controllers/profileController.js
+++ backend/controllers/profileController.js
@@ -59,7 +59,30 @@ const updateUserProfile = async (req, res) => {
   }
 };
 
+const getOtherAccount = async (req, res) => {
+  try {
+    const { username } = req.params;
+    const user = await User.findOne({ username }).select(
+      "-_id -password -email -phone",
+    );
+    if (!user) {
+      return res.status(404).json({
+        message: "Username Not Found",
+      });
+    }
+    return res.status(200).json({
+      message: "User found",
+      user: user,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+};
+
 module.exports = {
   getUserProfile,
   updateUserProfile,
+  getOtherAccount,
 };
--- backend/routes/profileRouter.js
+++ backend/routes/profileRouter.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const verifyToken = require("../middlewares/middleware");
-const { getUserProfile, updateUserProfile } = require("../controllers/profileController");
+const { getUserProfile, updateUserProfile, getOtherAccount } = require("../controllers/profileController");
 
 // protected route
 router.get("/profile", verifyToken, getUserProfile);
 router.put("/profile/update", verifyToken, updateUserProfile);
+router.get("/profile/:username", verifyToken, getOtherAccount)
 
 module.exports = router;
