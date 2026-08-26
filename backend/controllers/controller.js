ed42e3a30bd893917a0dced8cb58a17dcec97b7d
taman.js <tamanwit.25bcs10001@sst.scaler.com>
2026-08-26 12:08:06 +0530

    sending token to frontend

--- backend/controllers/controller.js
+++ backend/controllers/controller.js
@@ -110,6 +110,7 @@ const loginUser = async (req, res) => {
     return res.status(200).json({
       message: "Login successful",
       user: userResponse,
+      token : token,
     });
 
   } catch (error) {
